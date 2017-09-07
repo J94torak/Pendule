793e91dc44979ce90d8409fd68a5e21c9722ddeb
RTAI Linux Local User <rtai@pcl4105f.esiee.fr>
2017-09-07 16:41:29 +0200

    Start writing function ReadAD

--- 3718.c
+++ 3718.c
@@ -12,6 +12,7 @@ assert(in_channel<9);
 unsigned char v_channel=in_channel<<4+in_channel;
 outb(in_channel,MUX);
 }
+EXPORT_SYMBOL(SetChanel);
 
 void ADRangeSelect(unsigned char channel,unsigned char range){
 assert(range<9);
@@ -20,8 +21,15 @@ SetChanel(channel);
 outb(range,RANGE);
 SetChanel(v_channelBefore);
 }
+EXPORT_SYMBOL(ADRangeSelect);
 
 u16 ReadAD(void){
+while(inb(STATUS)&0b10000000!=0b00000000);
+outb(0xFF,R0);
+
+while(inb(STATUS)&0b10010000!=0b00010000);
+
 }
+EXPORT_SYMBOL(ReadAD);
 
 
