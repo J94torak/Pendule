ad5237548e22bc52a69da2df651639ef24673f92
RTAI Linux Local User <rtai@pcl4105f.esiee.fr>
2017-09-28 12:05:25 +0200

    Continue to add the possibility to record values

--- controller.c
+++ controller.c
@@ -18,6 +18,9 @@ double pasAngle;
 double origin;
 double angle0;
 
+int output_fd;    /* Input and output file descriptors */
+ssize_t  ret_out;    /* Number of bytes returned by read() and write() */
+char buffer[100];      /* Character buffer */
 
 void init_control(double pposition0,double pposition90,double porigin, double pangle_15, double pangle15, double pangle0){
 
@@ -33,7 +36,6 @@ void init_control(double pposition0,double pposition90,double porigin, double pa
 	angle15=pangle15;
 	pasPosition=(position0-position90)/POSITION_WIDTH;   //Volt/cm
     pasAngle= (angle_15-angle15)/ANGLE_WIDTH;          //volt/degre
-
     
 }
 
@@ -45,7 +47,7 @@ double commande(double angle, double position){
 	double x2k;
 	double x3k;
 	double x4k;
-	
+	double commande;
 
 	
 	x1k=x1;
@@ -58,7 +60,17 @@ double commande(double angle, double position){
 	x3=0.9140*x1k-0.3043*x2k+1.1306*x3k+0.2351*x4k-0.1149*angle+0.4000*position;
 	x4=-2.4476*x1k+0.0016*x2k-0.1546*x3k+0.7222*x4k+1.6173*angle-0.1150*position;
 
-	return -(-80.3092*x1-9.6237*x2-14.1215*x3-23.6260*x4);	
+	commande= -(-80.3092*x1-9.6237*x2-14.1215*x3-23.6260*x4);	
+
+  /*
+     ret_out = write (output_fd, &buffer, (ssize_t) ret_in);
+            if(ret_out != ret_in){
+                ///Write error 
+                perror("write");
+                return 4;
+            }*/
+    return commande;
+   
 
 }
 
@@ -85,16 +97,19 @@ return commande(conversionVoltToAngle(angle),conversionVoltToPosition(position))
 
 
 static int init_controller(void) {
-
-	init_control(POSITION0,POSITION90, ORIGIN, ANGLE_15,ANGLE15,ANGLE0);
+  output_fd = open("bigbrother.txt", O_CREAT, 0644);
+    if(output_fd == -1){
+        printk("[ERROR] Impossible to open bigbrother.txt\n");
+        }
+  init_control(POSITION0,POSITION90, ORIGIN, ANGLE_15,ANGLE15,ANGLE0);
   return 0;
 }
 
 
 
 static void exit_controller(void) {
+ close (output_fd);
 }
-
 
 module_init(init_controller);
 module_exit(exit_controller);
